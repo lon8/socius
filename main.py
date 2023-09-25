a50fe30346ff06eb7eb1ed19f035905e0885818f
lon8 <vlad.kanatik@ya.ru>
2023-09-25 14:36:43 +0300

    main_version. site is done

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ def get_application():
 app = get_application()
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000, log_level='info')
+    uvicorn.run(app, host='127.0.0.1', port=80, log_level='info')
